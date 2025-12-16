af4861b8e3d116833796c275bf36a0ffaa84f64e
Harsh <harshsingh20hzb@gmail.com>
2025-12-17 01:03:50 +0530

    first commit

---
+++ 6.3/6.3.Project.js
@@ -0,0 +1,36 @@
+var tl=gsap.timeline()
+
+tl.from("#nav h3",{
+    y:-50,
+    opacity:0,
+    delay:0.3,
+    duration:0.8,
+    stagger:0.2,
+})
+
+tl.from("#main h1",{
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4,
+})
+
+tl.from("img",{
+    x:100,
+    rotate:360,
+    opacity:0,
+    duration:0.5,
+    stagger:0.7,
+})
+tl.from("#second",{
+    y:300,
+    opacity:0,
+    duration:1,
+    // stagger:0.7,
+})
+tl.from("#first",{
+    y:300,
+    opacity:0,
+    duration:1,
+    stagger:0.7,
+})
